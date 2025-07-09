ef8b643564f7e5847a1712b7b19cfc44abceadc4
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-07-09 19:34:03 +0200

    Dodanie funkcji połączonej z wyrażeniami JSX.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+
+function isAdult(age) {
+  return age >= 18? "TAK" : "NIE";
+}
+
+function formatting(text) {
+  return text.toLowerCase().charAt().toUpperCase() + text.slice(1).toLowerCase()
+};
+
 const list = [
-  { "firstname": "Adam", "lastname": "Kowalski", "age": "33"},
-  { "firstname": "Izabela", "lastname": "Makowska", "age": "25"},
-  { "firstname": "Jan", "lastname": "Urbański", "age": "46"}
+  { "firstname": "aDAM", "lastname": "kowalski", "age": "10"},
+  { "firstname": "IZabela", "lastname": "MAKOWSKA", "age": "15"},
+  { "firstname": "Jan", "lastname": "UrBański", "age": "46"}
 ];
 
 function App() {
@@ -13,21 +22,25 @@ function App() {
           <th>Imię</th>
           <th>Nazwisko</th>
           <th>Wiek</th>
+          <th>Pełnoletni?</th>
         </tr>
         <tr>
-          <td>{list[0].firstname}</td>
-          <td>{list[0].lastname}</td>
+          <td>{formatting(list[0].firstname)}</td>
+          <td>{formatting(list[0].lastname)}</td>
           <td>{list[0].age}</td>
+          <td>{isAdult(list[0].age)}</td>
         </tr>
         <tr>
-          <td>{list[1].firstname}</td>
-          <td>{list[1].lastname}</td>
+          <td>{formatting(list[1].firstname)}</td>
+          <td>{formatting(list[1].lastname)}</td>
           <td>{list[1].age}</td>
+          <td>{isAdult(list[1].age)}</td>
         </tr>
         <tr>
-          <td>{list[2].firstname}</td>
-          <td>{list[2].lastname}</td>
+          <td>{formatting(list[2].firstname)}</td>
+          <td>{formatting(list[2].lastname)}</td>
           <td>{list[2].age}</td>
+          <td>{isAdult(list[2].age)}</td>
         </tr>
       </table>
     </div>
