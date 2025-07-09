1e8a966361fd6adbf284af6f6eeb8b1c87ee07a4
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-07-09 14:46:47 +0200

    Pierwsza modyfikacja App.js i App.css dodanie tabeli.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <table cellPadding="0" cellSpacing="0">
+        <tr>
+          <th>Imię</th>
+          <th>Nazwisko</th>
+          <th>Wiek</th>
+        </tr>
+        <tr>
+          <td>Adam</td>
+          <td>Kowalski</td>
+          <td>33</td>
+        </tr>
+        <tr>
+          <td>Izabela</td>
+          <td>Makowska</td>
+          <td>25</td>
+        </tr>
+        <tr>
+          <td>Jan</td>
+          <td>Urbański</td>
+          <td>46</td>
+        </tr>
+      </table>
     </div>
   );
 }
