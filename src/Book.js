6e7a2a10536bfb6cee6da7a2d042f87a009d9074
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-07-10 10:50:48 +0200

    Refaktoryzacja kodu

--- src/Book.js
+++ src/Book.js
@@ -1,27 +1,14 @@
 import React, { Component } from "react";
+import BookDescription from "./BookDescription";
 import Heder from "./Heder";
 import Footer from "./Footer";
 
 class Book extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            book: {
-                title: "Hobbit",
-                author: "J. R. Tolkien",
-                pages: "455"
-            }
-            
-        }
-    };
-
     render() {
         return (
             <div>
                 <Heder />
-                <h1>{this.state.book.title}</h1>
-                <h2>{this.state.book.author}</h2>
-                <h4>Pages: {this.state.book.pages}</h4>
+                <BookDescription />
                 <Footer />
             </div>
         );
