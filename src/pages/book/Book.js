27719500db7398a89cd9d810bc4f51d73915c94e
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-07-10 14:05:15 +0200

    Użycie Props

--- src/pages/book/Book.js
+++ src/pages/book/Book.js
@@ -4,11 +4,45 @@ import Heder from "../../components/partial/Heder";
 import Footer from "../../components/partial/Footer";
 
 class Book extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    title: "Hobbit",
+                    author: "J. R. Tolkien",
+                    pages: "455"
+                },
+                                {
+                    title: "Rok 1984",
+                    author: "G. Orwel",
+                    pages: "455"
+                },
+                                {
+                    title: "Władca Pierścieni",
+                    author: "J. R. Tolkien",
+                    pages: "669"
+                },
+                                {
+                    title: "Stary człowiek i morze",
+                    author: "E. Hemigway",
+                    pages: "226"
+                }
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <Heder />
-                <BookDescription />
+                {
+                    this.state.books.map((book, i) =>
+                    <BookDescription key={i} title={book.title} author={book.author} pages={book.pages} />
+                    )
+                    
+                }
+                
                 <Footer />
             </div>
         );
